bbea9f1ae6af95f46c154657f12fdaac6cefbcfb
20018318 <20018318@myrp.edu.sg>
2021-05-03 15:13:52 +0800

    edit

--- app/src/main/java/sg/edu/rp/c346/id20018318/billplease/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20018318/billplease/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextClock;
 import android.widget.TextView;
@@ -21,10 +22,12 @@ public class MainActivity extends AppCompatActivity {
     ToggleButton gst;
     EditText discount;
     RadioGroup rgPayment;
+    RadioButton cash;
     Button split;
     Button reset;
     TextView totalBill;
     TextView eachPays;
+    TextView errorMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,21 +37,23 @@ public class MainActivity extends AppCompatActivity {
         amount = findViewById(R.id.editTextAmount);
         numPax = findViewById(R.id.editTextPax);
         svs = findViewById(R.id.toggleButtonSvs);
-        svs = findViewById(R.id.toggleButtonGst);
+        gst = findViewById(R.id.toggleButtonGst);
         discount = findViewById(R.id.editTextDiscount);
         rgPayment = findViewById(R.id.radioGroupPayment);
+        cash = findViewById(R.id.radioButtonCash);
         split = findViewById(R.id.buttonSplit);
         reset = findViewById(R.id.buttonReset);
         totalBill = findViewById(R.id.textViewBill);
         eachPays = findViewById(R.id.textViewEach);
+        errorMsg = findViewById(R.id.textViewError);
 
         split.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (amount.getText().toString().length() != 0 && numPax.getText().toString().length() != 0) {
+                if (amount.getText().toString().trim().length() > 0 && numPax.getText().toString().trim().length() > 0) {
                     double newAmt = 0.0;
                     if (!svs.isChecked() && !gst.isChecked()) {
-                       newAmt = Double.parseDouble(amount.getText().toString());
+                        newAmt = Double.parseDouble(amount.getText().toString());
                     } else if (svs.isChecked() && !gst.isChecked()) {
                         newAmt = Double.parseDouble(amount.getText().toString()) * 1.1;
                     } else if (!svs.isChecked() && gst.isChecked()) {
@@ -57,21 +62,24 @@ public class MainActivity extends AppCompatActivity {
                         newAmt = Double.parseDouble(amount.getText().toString()) * 1.17;
                     }
 
-                    if (discount.getText().toString().length() != 0) {
-                        newAmt = 1 - (newAmt * Double.parseDouble(discount.getText().toString())) / 100;
+                    if (discount.getText().toString().trim().length() > 0) {
+                        newAmt = newAmt * (1 -  (Double.parseDouble(discount.getText().toString()) / 100));
                     }
 
                     totalBill.setText("Total Bill: $" + String.format("%.2f", newAmt));
 
                     int newPax = Integer.parseInt(numPax.getText().toString());
-                    if (newPax != 1) {
+                    if (newPax > 0) {
                         int checkedRadioId = rgPayment.getCheckedRadioButtonId();
                         if (checkedRadioId == R.id.radioButtonCash) {
-                            eachPays.setText("Each Pays: $" + String.format("%.2f", (newAmt / newPax), "%s", " via Cash"));
+                            eachPays.setText("Each Pays: $" + String.format("%.2f", newAmt / newPax) + " via Cash");
                         } else {
-                            eachPays.setText("Each Pays: $" + String.format("%.2f", (newAmt / newPax), "%s", " Paynow to 86321944"));
+                            eachPays.setText("Each Pays: $" + String.format("%.2f", newAmt / newPax) + " Paynow to 86321944");
                         }
                     }
+
+                } else {
+                    errorMsg.setText("Please fill in all blanks.");
                 }
             }
         });
@@ -84,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
                 svs.setChecked(false);
                 gst.setChecked(false);
                 discount.setText("");
+                cash.setChecked(true);
             }
         });
     }
